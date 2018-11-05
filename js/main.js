37f976b44c75b3994d644e519dd15b9c4a03a7fb
Moroj-Alharbi <Moroj.alh@gmail.com>
2018-11-06 01:45:48 +0300

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,14 +1,35 @@
 // console.log("Up and running!");
 
-var cards = ["queen", "queen", "king", "king"];
+// var cards = ["queen", "queen", "king", "king"];
+var cards = [{
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png"
+    },
+    {
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+    },
+    {
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png"
+    },
+    {
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds.png"
+    },
+]
 
 var cardsInPlay = [];
 
 var checkForMatch = function(){
     if (cardsInPlay[0] === cardsInPlay[1]) {
-        console.log("You found a match!");
+        alert("You found a match!");
         } else {
-        console.log("Sorry, try again.");
+            alert("Sorry, try again.");
         }
 }
 
@@ -21,9 +42,11 @@ var flipCard = function(cardId){
     // cardsInPlay.push(cardTwo);
     // console.log('User flipped king');
     
-    console.log('User flipped '+ cards[cardId] );
-    cardsInPlay.push(cards[cardId]);
-    
+    console.log('User flipped '+ cards[cardId].rank );
+    cardsInPlay.push(cards[cardId].rank);
+    console.log(cards[cardId].cardImage );
+    console.log(cards[cardId].suit );
+
     checkForMatch();
 }
 flipCard(0);
